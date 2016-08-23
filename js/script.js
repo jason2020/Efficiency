4c92dfe530736fb26e7aa06d7a2406d2255eb7d5
jason2020 <jtay20@punahou.edu>
2016-08-23 11:17:19 -1000

    worked on basic functionality

--- js/script.js
+++ js/script.js
@@ -4,30 +4,36 @@ function checkSupport() {
 		console.log("localStorage avaliable");
 		formatPage(data);
 	} else {
-		alert("Browser lacks support for localStorage. Please use another browser.");
+		alert("Browser lacks support for localStorage. Please use another browser.")
+		console.error("No browser support.")
 	}
 }
 
 function getData() {
 	var visited = sessionStorage.getItem("visited");
+	// default settings
+	var defaultData = {
+		list1: "test",
+		tempPref: "celcius",
+		clock: "12",
+		style: "default"
+	};
 	if (visited == null) {
 		sessionStorage.setItem("visited", true);
 		console.log("New page");
-		// default settings
-		var data = {
-			"list": ["test", "test"],
-			"tempPref": "celcius",
-			"style": ["default", "normal"]
-		};
-		sessionStorage.setItem("list", data.list);
-		sessionStorage.setItem("tempPref", data.tempPref);
-		sessionStorage.setItem("style", data.style);
-		return data;
+		setItems(defaultData);
+		// sessionStorage.setItem("list", data.list);
+		// sessionStorage.setItem("tempPref", data.tempPref);
+		// sessionStorage.setItem("clock", data.clock);
+		// sessionStorage.setItem("style", data.style);
+		return defaultData;
 	} else {
-		var data = {};
-		data.list = sessionStorage.getItem("list").split(",");
-		data.tempPref = sessionStorage.getItem("tempPref");
-		data.style = sessionStorage.getItem("style").split(",");
+		// var data = getItems(defaultData);
+		// var data = {};
+		// data.list = sessionStorage.getItem("list1");
+		// data.tempPref = sessionStorage.getItem("tempPref");
+		// data.clock = sessionStorage.getItem("clock");
+		// data.style = sessionStorage.getItem("style");
 		return data;
 	}
 }
@@ -36,5 +42,31 @@ checkSupport();
 
 function formatPage (obj) {
 	console.log(obj);
-	
+	setTime(obj.clock)
+}
+
+function setTime(timePref) {
+	var date = new Date(),
+		day = date.getDate();
+
+}
+
+function setItems(obj) {
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop)) {
+			sessionStorage.setItem(prop, obj.prop);
+		}
+	}
+}
+
+function getItems(obj) {
+	var tObj = {};
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop)) {
+			var objProperty = sessionStorage.getItem("list1");
+			console.log(objProperty);
+			tObj[prop] = objProperty;
+		}
+	}
+	return tObj;
 }
